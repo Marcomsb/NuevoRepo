441d2102868b3450ff66c6ce5be2843e1f873e22
Marcomsb <msegura_86_12@hotmail.com>
2015-05-18 20:23:33 -0600

    Primer cambio

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
+
     class Program
     {
+        static int num = 1 + 1;   
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hola Mundo!");
+            System.Console.WriteLine(""+ num);
             System.Console.ReadKey();
         }
     }
