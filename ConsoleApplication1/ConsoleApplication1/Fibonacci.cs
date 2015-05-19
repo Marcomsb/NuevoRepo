57b060b0445b94bec432d63fd16fe90c733c692f
Marcomsb <msegura_86_12@hotmail.com>
2015-05-18 21:08:15 -0600

    Fibonacci

---
+++ ConsoleApplication1/ConsoleApplication1/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Fibonacci
+    {
+        public int calcular(int entero)
+        {
+          
+            if (entero == 1 || entero == 2)
+            {
+               
+                return 1;
+            }
+            else {
+                return calcular(entero - 2) + calcular(entero - 1); 
+            }
+
+            return 0;
+            
+            
+            }
+    }
+}
