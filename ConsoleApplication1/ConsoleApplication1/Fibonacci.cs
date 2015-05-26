561aac93ef71f53195fcf1c1786800fc3dd3486d
Marcomsb <msegura_86_12@hotmail.com>
2015-05-25 20:39:08 -0600

    Fibonacci_Arrays

--- ConsoleApplication1/ConsoleApplication1/Fibonacci.cs
+++ ConsoleApplication1/ConsoleApplication1/Fibonacci.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,32 @@ namespace ConsoleApplication1
     {
         public int calcular(int entero)
         {
-          
+
             if (entero == 1 || entero == 2)
             {
-               
+
                 return 1;
             }
-            else {
-                return calcular(entero - 2) + calcular(entero - 1); 
+            else
+            {
+                return calcular(entero - 2) + calcular(entero - 1);
             }
 
             return 0;
-            
-            
-            }
+
+
+        }
+        public ArrayList calculaArray(IEnumerable myList)
+        {
+            ArrayList result = new ArrayList();
+            foreach (int obj in myList)
+            {
+
+                result.Add(calcular(obj));
+               
+            } return result;
+        } 
     }
 }
+
+
--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,12 +16,36 @@ namespace ConsoleApplication1
             System.Console.WriteLine("Hola Mundo!");
             System.Console.WriteLine(""+ num);
             Fibonacci n = new Fibonacci();
-            System.Console.WriteLine("El número de Fibonacci 1 es: " + n.calcular(1));
+            ArrayList ArrayFibo = new ArrayList();
+            int[] numbers; //Aquí se crea el array de Integers
+            numbers = new int[6]{4,6,20,1,11,25};  // Lleno el Array de 6 números
+            /*System.Console.WriteLine("El número de Fibonacci 1 es: " + n.calcular(1));
             System.Console.WriteLine("El número de Fibonacci 1 es: " + n.calcular(2));
-            System.Console.WriteLine("El número de Fibonacci 1 es: " + n.calcular(7));
+            System.Console.WriteLine("El número de Fibonacci 1 es: " + n.calcular(7));*/
+            ArrayFibo.Add(4);
+            ArrayFibo.Add(6);
+            ArrayFibo.Add(20);
+            ArrayFibo.Add(1);
+            ArrayFibo.Add(11);
+            ArrayFibo.Add(25);
+            
+            Console.WriteLine("Array Fibonacci");
+            PrintValues(ArrayFibo);
+            Console.WriteLine("Resultado de Fibonacci");
+            PrintValues(n.calculaArray(ArrayFibo));
+            PrintValues(n.calculaArray(numbers));
+            
+
             System.Console.ReadKey();
             
 
+        }
+        public static void PrintValues(IEnumerable myList)
+        {
+            
+            foreach (Object obj in myList)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
         }
     }
 }
