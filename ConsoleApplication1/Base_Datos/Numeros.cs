98236aa4c97f29a15156ab2491222b4631cd9f58
Marcomsb <msegura_86_12@hotmail.com>
2015-06-01 20:20:19 -0600

    Solucionado 2.0

---
+++ ConsoleApplication1/Base_Datos/Numeros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Datos
+{
+    public class Numeros
+    {
+        private List<int> Nums = new List<int>();
+        public int Agregar(int numrecibido)
+        {
+            Nums.Add(numrecibido);
+            
+            return 0;
+        }
+        public int ObtenerCantidad()
+        {
+            int Cantidad = 0;
+            foreach (int obj in Nums)
+            {
+                
+
+                ++Cantidad;
+
+        }
+            return Nums.Count; 
+            //return Cantidad;
+    }
+ }
+}
